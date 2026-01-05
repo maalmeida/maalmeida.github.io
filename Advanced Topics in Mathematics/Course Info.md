2c3b7c931f171c7889b7437b58ce8d6df59e37f6
maalmeida <maalmeida85@gmail.com>
2026-01-04 19:51:46 -0500

    new commit

---
+++ Advanced Topics in Mathematics/Course Info.md
@@ -0,0 +1,4 @@
+# Quiz-01
+Epsilon-Delta proofs: (1) Basic (2) Hole (3) Quadratic
+
+# Quiz-02
---
+++ Advanced Topics in Mathematics/Linear Algebra/1.7/Untitled-1.sage.py
@@ -0,0 +1,6 @@
+v1 = vector([1, 2, 3])
+v2 = vector([4, 5, 6])
+v3 = vector([7, 8, 9])
+
+V = [v1, v2, v3]
+print(V)
